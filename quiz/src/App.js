196bc7b0c64770e144cc0756ca872d88efa1ebb1
Teeratham Vitchutripop <teerathamvitchutripop@d-172-26-66-209.dhcp.virginia.edu>
2021-04-22 00:55:05 -0400

    Questions now change color depending on whether the correct answer was chosen or not

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import './App.css'
 export default function App() {
   const [questions, setQuestions] = useState([]);
   const numOfQs = 10;
@@ -11,58 +12,53 @@ export default function App() {
       });
   }, []);
 
-  //Fisher-Yates Shuffle
-  const shuffle = (array) => {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
-  };
+  function Question(props){
 
-  const randomizeAnswers = (a) =>{
-      let answerList = [];
-      answerList.push(a.correct_answer);
-      let x = 0;
-      for(x=0;x<a.incorrect_answers.length;x++)
-        answerList.push(a.incorrect_answers[x]);
-      answerList=shuffle(answerList);
-      if(answerList.length==2)
-        return (<div>
-            <button>{answerList[0]}</button>
-            <button>{answerList[1]}</button>
-        </div>);
-      if(answerList.length===3)
-        return (<div>
-          <button>{answerList[0]}</button>
-          <button>{answerList[1]}</button>
-          <button>{answerList[2]}</button>
-      </div>);
-      if(answerList.length===4)
-        return (<div>
-          <button>{answerList[0]}</button>
-          <button>{answerList[1]}</button>
-          <button>{answerList[2]}</button>
-          <button>{answerList[3]}</button>
-      </div>);
-    };
+    const[answerList,setAnswerList]=useState([]);
+    const[qColor, setQColor]=useState({color:'black'});
+    const[firstRun,setFirstRun]=useState(true);
+      if(firstRun===true){
+        answerList.push(props.question.correct_answer);
+        let x = 0;
+        for(x=0;x<props.question.incorrect_answers.length;x++)
+        answerList.push(props.question.incorrect_answers[x]);
+        let currentIndex = answerList.length, temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = answerList[currentIndex];
+          answerList[currentIndex] = answerList[randomIndex];
+          answerList[randomIndex] = temporaryValue;
+          setFirstRun(false);
+        }
+      }
 
-
-    return (
+    return(
       <div>
-        <h1>Quiz Time! 👋</h1>
-        {questions.map((question) => (
-          <div class="qAndA">
-          <p>{question.question}</p>
-          {randomizeAnswers(question)}
-          </div>
-        ))}
+        <p style={qColor}>{props.question.question}</p>
+        {answerList.map(answers => (<button onClick={()=>
+        {if(answers===props.question.correct_answer){
+            console.log("Correct Answer Chosen");
+           setQColor({color:'green'});
+        }
+          else{
+            console.log("Incorrect Answer Chosen");
+           setQColor({color:'red'});
+          }
+        }}>{answers}</button>))}
       </div>
+
     );
 
   };
 
+  return (
+    <div class="centered">
+      <h1>👾 Quiz Time! 🤖</h1>
+      {questions.map((question) => (
+        <Question question={question}/>
+      ))}
+    </div>
+  );
+
+};
